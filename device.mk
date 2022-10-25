308afb782e36c3f09765144d474b3d62dba4e2e6
Blackmanx <blackman@sajl.tk>
2022-10-25 21:18:33 +0200

    bitra: configs: add msm_irqbalance
    Blacklist recommended irqs as well and add the correct ones

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# IRQ Balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Overlays
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
