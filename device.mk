5bc9b8d3116b10804104488eeaa4f42274c899d2
Krish <drkrish.20@gmail.com>
2023-02-19 22:46:49 +0530

    Prebuilts: Added RiceDroid Dialer & RiceDroid Contacts

--- device.mk
+++ device.mk
@@ -57,6 +57,12 @@ PRODUCT_PACKAGES += \
 #maintainer_overlay    
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
+#Rice_Dialer
+PRODUCT_PACKAGES += \
+Dialer
+#Rice_Contacts
+PRODUCT_PACKAGES += \
+Contacts
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     device/realme/bitra
